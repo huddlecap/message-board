b099bb256f88d39729dde5642e3419cc9e515f0e
Probin <huddlecap@gmail.com>
2026-04-28 16:27:25 +0530

    downgrade prisma to v5

--- server/src/prisma.js
+++ server/src/prisma.js
@@ -1,9 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
-const { PrismaPg } = require("@prisma/adapter-pg");
 
-require("dotenv").config();
-
-const adapter = new PrismaPg(process.env.DATABASE_URL);
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 module.exports = prisma;
