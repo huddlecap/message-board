4e854be48ce7c01b0266f824c74d6868d5ea1c8e
Probin <huddlecap@gmail.com>
2026-04-28 10:36:10 +0530

    fix api url for production

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+baseURL: "https://message-board-production-220e.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
